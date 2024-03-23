b771fbe457d0aeab20a85328faa7d2e72b80d00f
codingbeans <beans@kryptogo.com>
2024-03-23 19:12:25 +0800

    Update deploy script

--- contracts/script/Paymaster.s.sol
+++ contracts/script/Paymaster.s.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract DeployPaymasterScript is Script {
     // Define an enum for supported chain IDs
-    enum Chain {
+    enum ChainEnum {
         Sepolia,
         OpSepolia,
         ArbSepolia,
@@ -25,36 +25,38 @@ contract DeployPaymasterScript is Script {
     // Mapping of enum Chain to their configurations
     mapping(uint256 => ChainConfig) public chainConfigs;
 
+    // Mapping of enum Chain to their chain IDs
+    mapping(ChainEnum => uint256) public chainIDs;
+
     function setUp() public {
         // Mapping enum values to chain IDs
-        mapping(Chain => uint256) chainIDs;
-        chainIDs[Chain.Sepolia] = 11155111;
-        chainIDs[Chain.OpSepolia] = 11155420;
-        chainIDs[Chain.ArbSepolia] = 421614;
-        chainIDs[Chain.PolygonMumbai] = 80001;
-        chainIDs[Chain.ScrollSepolia] = 534351;
-        chainIDs[Chain.Zircuit] = 48899;
+        chainIDs[ChainEnum.Sepolia] = 11155111;
+        chainIDs[ChainEnum.OpSepolia] = 11155420;
+        chainIDs[ChainEnum.ArbSepolia] = 421614;
+        chainIDs[ChainEnum.PolygonMumbai] = 80001;
+        // chainIDs[ChainEnum.ScrollSepolia] = 534351;
+        // chainIDs[ChainEnum.Zircuit] = 48899;
 
         // Initialize configurations for each chain
-        chainConfigs[chainIDs[Chain.Sepolia]] = ChainConfig({
+        chainConfigs[chainIDs[ChainEnum.Sepolia]] = ChainConfig({
             tokenOracleAddress: 0x694AA1769357215DE4FAC081bf1f309aDC325306,
             tokenAddress: 0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238,
-            nativeOracleAddress: 0x0
+            nativeOracleAddress: address(0x0)
         });
-        chainConfigs[chainIDs[Chain.OpSepolia]] = ChainConfig({
+        chainConfigs[chainIDs[ChainEnum.OpSepolia]] = ChainConfig({
             tokenOracleAddress: 0x61Ec26aA57019C486B10502285c5A3D4A4750AD7,
             tokenAddress: 0x5fd84259d66Cd46123540766Be93DFE6D43130D7,
-            nativeOracleAddress: 0x0
+            nativeOracleAddress: address(0x0)
         });
-        chainConfigs[chainIDs[Chain.ArbSepolia]] = ChainConfig({
+        chainConfigs[chainIDs[ChainEnum.ArbSepolia]] = ChainConfig({
             tokenOracleAddress: 0xd30e2101a97dcbAeBCBC04F14C3f624E67A35165,
             tokenAddress: 0x75faf114eafb1BDbe2F0316DF893fd58CE46AA4d,
-            nativeOracleAddress: 0x0
+            nativeOracleAddress: address(0x0)
         });
-        chainConfigs[chainIDs[Chain.PolygonMumbai]] = ChainConfig({
+        chainConfigs[chainIDs[ChainEnum.PolygonMumbai]] = ChainConfig({
             tokenOracleAddress: 0x0715A7794a1dc8e42615F059dD6e406A6594651A,
             tokenAddress: 0x9999f7Fea5938fD3b1E26A12c3f2fb024e194f97,
-            nativeOracleAddress: 0x0
+            nativeOracleAddress: address(0x0)
         });
     }
 
@@ -62,7 +64,7 @@ contract DeployPaymasterScript is Script {
         vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
         address deployerAddress = vm.envAddress("ETH_FROM");
 
-        uint256 chainId = vm.chainId();
+        uint256 chainId = block.chainid;
         console.log("Deploying to chainId:", chainId);
 
         ChainConfig memory config = chainConfigs[chainId];
@@ -71,24 +73,24 @@ contract DeployPaymasterScript is Script {
             "Chain configuration not set"
         );
 
-        TokenPaymasterConfig
-            memory tokenPaymasterConfig = TokenPaymasterConfig({
+        TokenPaymaster.TokenPaymasterConfig
+            memory tokenPaymasterConfig = TokenPaymaster.TokenPaymasterConfig({
                 priceMarkup: 1e26,
-                minEntryPointBalance: 1e18,
                 refundPostopCost: 50000,
                 priceMaxAge: 2000000
             });
 
-        OracleHelperConfig memory oracleHelperConfig = OracleHelperConfig({
-            cacheTimeToLive: 86400,
-            maxOracleRoundAge: 2000000,
-            tokenOracle: IOracle(config.tokenOracleAddress), // Cast the address to IOracle
-            nativeOracle: IOracle(config.nativeOracleAddress), // Cast the address to IOracle
-            tokenToNativeOracle: true,
-            tokenOracleReverse: false,
-            nativeOracleReverse: false,
-            priceUpdateThreshold: 1e20
-        });
+        OracleHelper.OracleHelperConfig memory oracleHelperConfig = OracleHelper
+            .OracleHelperConfig({
+                cacheTimeToLive: 86400,
+                maxOracleRoundAge: 2000000,
+                tokenOracle: IOracle(config.tokenOracleAddress), // Cast the address to IOracle
+                nativeOracle: IOracle(config.nativeOracleAddress), // Cast the address to IOracle
+                tokenToNativeOracle: true,
+                tokenOracleReverse: false,
+                nativeOracleReverse: false,
+                priceUpdateThreshold: 1e20
+            });
 
         IEntryPoint entryPoint = IEntryPoint(
             0x0000000071727De22E5E9d8BAf0edAc6f37da032
