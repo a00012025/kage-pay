958b0067f976d536b3e8b949222700475c7b7390
charles chien <charleschien7996@gmail.com>
2024-03-23 14:49:48 +0800

    feat: add user operation

--- app/lib/features/payment/application/payment_service.dart
+++ app/lib/features/payment/application/payment_service.dart
@@ -1,7 +1,11 @@
+import 'dart:math';
 
 import 'package:app/features/payment/application/payment_exception.dart';
+import 'package:app/features/payment/domain/user_operation.dart';
 import 'package:app/features/payment/domain/utxo_address.dart';
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+import 'package:web3dart/web3dart.dart';
 
 class PaymentService {
   List<UtxoAddress> getAddressesToSend({
@@ -32,5 +36,19 @@ class PaymentService {
     }
 
     return selectedAddresses;
+  }
+
+  List<UserOperation> getUserOperations() {
+    return [];
+  }
+
+  String sendUserOperations(List<UserOperation> userOperations) {
+    // Generate a new private key
+    var rng = Random.secure();
+    Credentials random = EthPrivateKey.createRandom(rng);
+    var httpClient = http.Client();
+    var client = Web3Client('https://1rpc.io/sepolia', httpClient);
+
+    return '';
   }
 }
